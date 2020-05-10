ee34d482b64fc2952ec088184ae6221d5a500eef
Raul Mena <raulmenamontes@gmail.com>
2020-05-10 11:35:40 +0200

    Draw triangle with legacy opengl

--- src/Application.cpp
+++ src/Application.cpp
@@ -25,6 +25,13 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        //draw triangle with lecagy opengl
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f( 0.0f,  0.5f);
+        glVertex2f( 0.5f, -0.5f);
+        glEnd();
+
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
 
