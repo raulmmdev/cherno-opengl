f528f13253568def3adbc5736676bf58f9ccb651
Raul Mena <raulmenamontes@gmail.com>
2020-05-13 19:18:57 +0200

    Deal with errors creating a macro for add breakpoing also functs for display the file and line and for cleaning erros previously

--- src/Application.cpp
+++ src/Application.cpp
@@ -6,6 +6,31 @@
 #include <string>
 #include <sstream>
 
+#define DEBUG 1;
+
+#ifdef DEBUG
+    #define ASSERT(x) if (!(x)) __debugbreak();
+    #define GLCall(x) GLClearError();x;ASSERT(GLLogCall(#x,__FILE__,__LINE__))
+#else
+    #define GLCall(x)x
+#endif
+
+static void GLClearError()
+{
+    while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
+
 struct ShaderProgramSource
 {
     std::string VertexSource;
@@ -150,7 +175,8 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         // number of inidces
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
+        //GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
