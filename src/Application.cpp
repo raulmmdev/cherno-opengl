0891f0dccf6743f2cd9d0e40bff40ecab2f86dec
Raul Mena <raulmenamontes@gmail.com>
2020-05-15 17:14:49 +0200

    Add a uniform for color and send the value from the CPU

--- src/Application.cpp
+++ src/Application.cpp
@@ -133,6 +133,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error" << std::endl;
 
@@ -168,11 +170,27 @@ int main(void)
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
     GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
+
+        GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+            r += increment;
 
         // number of inidces
         // for check an error GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
