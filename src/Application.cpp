2dcaa4959d2200d9edf0833f963992ca689473e0
Raul Mena <raulmenamontes@gmail.com>
2020-05-22 11:59:15 +0200

    Add basic renderer for extract more code

--- src/Application.cpp
+++ src/Application.cpp
@@ -11,6 +11,7 @@
 #include "Renderer.h"
 #include "IndexBuffer.h"
 #include "VertexBuffer.h"
+#include "VertexBufferLayout.h"
 #include "VertexArray.h"
 #include "Shader.h"
 
@@ -77,6 +78,8 @@ int main(void)
         ib.Unbind();
         shader.Unbind();
 
+        Renderer renderer;
+
         float r = 0.0f;
         float increment = 0.05f;
 
@@ -84,17 +87,12 @@ int main(void)
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT));
+            renderer.Clear();
 
             shader.Bind();
             shader.SetUniform4f("u_Color", r, 0.3f, 0.8f, 1.0f);
 
-            va.Bind();
-            ib.Bind();
-
-            // number of inidces
-            // for check an error GLCall(glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr));
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+            renderer.Draw(va, ib, shader);
 
             if (r > 1.0f)
                 increment = -0.05f;
