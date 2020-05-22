72bf84921e3154b36180f0baa7e7426697080620
Raul Mena <raulmenamontes@gmail.com>
2020-05-22 15:09:28 +0200

    Create SetUniformMat4f

--- src/Shader.h
+++ src/Shader.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <unordered_map>
 
+#include <glm/glm.hpp>
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -31,5 +33,6 @@ public:
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform1i(const std::string& name, int value);
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 };
 
